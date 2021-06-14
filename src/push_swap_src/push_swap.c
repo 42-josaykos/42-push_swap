2f16e0ccd12e21b8bec15d123417a26d99027872
Jonny Saykosy <josayko@pm.me>
2021-06-14 11:03:05 +0200

    fixed leaks

--- src/push_swap_src/push_swap.c
+++ src/push_swap_src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jonny <josaykos@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 16:38:15 by jonny             #+#    #+#             */
-/*   Updated: 2021/06/14 10:38:19 by jonny            ###   ########.fr       */
+/*   Updated: 2021/06/14 10:56:42 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ void	push_swap(t_lst *stack_a)
 
 	stack_b = NULL;
 	ft_bzero(buf, BUF_SIZE);
-	init_stack(&stack_b, NULL);
 	if (is_sorted(stack_a, ASCENDING))
 		return ;
+	init_stack(&stack_b, NULL);
 	get_median(stack_a);
 	if (stack_a->len <= 3)
 		sort_3numbers(stack_a, buf);
@@ -41,8 +41,7 @@ void	push_swap(t_lst *stack_a)
 	{
 		sort_a(stack_a, stack_b, buf);
 		stack_a = sort_b(stack_a, stack_b, buf);
-		free_stack(stack_b);
-		stack_b = NULL;
 	}
+	free_stack(stack_b);
 	ft_putstr_fd(buf, STDOUT_FILENO);
 }
